284adee24979b33a9d817e29bf31bf512501abec
ashutosh3216 <git@stackblitz.com>
2025-02-11 10:36:14 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('static'));
+
+app.get('/shout', (req, res) => {
+  let name=req.query.name;
+  let uppercasename=name.toUpperCase();
+  res.send(uppercasename);
+});
+
+app.get('/fullname',(req,res)=>{let fname=req.query.fname; let lname = req.query.lname; res.send(fname+lname)});
+
+app.get('/date',(req,res)=>{let month=req.query.month;let year =req.query.year; res.send(month+', '+year)});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
